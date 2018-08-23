b29479abe590ac1ea95c899060e7bab3f95bafb0
Rohit Surwase <rohit@getwalnut.com>
2018-08-23 08:04:32 +0530

    Initial Commit

--- Architecture-Components-Kotlin-Basic/app/src/main/java/com/rohitss/aac/DatabaseDAO.kt
+++ Architecture-Components-Kotlin-Basic/app/src/main/java/com/rohitss/aac/DatabaseDAO.kt
@@ -2,6 +2,9 @@ package com.rohitss.aac
 
 import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.doAsyncResult
+import org.jetbrains.anko.uiThread
 
 /**
  * Created by Rohit Surwase on 07/08/18.
@@ -10,6 +13,16 @@ import android.arch.persistence.room.Dao
 @Dao
 interface DatabaseDAO {
     fun getAllNewsDAO(): LiveData<List<ArticlesItem>>? {
+        doAsync {
+
+            uiThread {  }
+        }
+
+        doAsyncResult {
+
+            uiThread {  }
+        }
+
         return null
     }
 }
